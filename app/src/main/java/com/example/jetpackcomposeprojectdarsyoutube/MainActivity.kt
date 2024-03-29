6308fb8619e40508bfa2e4ce39d1956bd5946a2c
Umedjon Saidulloev <umedjon.saidulloev@mail.ru>
2024-03-29 11:36:17 +0500

    Lesson 3

--- app/src/main/java/com/example/jetpackcomposeprojectdarsyoutube/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeprojectdarsyoutube/MainActivity.kt
@@ -3,60 +3,82 @@ package com.example.jetpackcomposeprojectdarsyoutube
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposeprojectdarsyoutube.ui.theme.JetpackComposeProjectDarsYouTubeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            Column {
+                ListItem(name = "Vali Akhmedov", prof = "UX/UI")
+                ListItem(name = "Adbu Rahimov", prof = "PHP DEVELOPER")
+                ListItem(name = "Dilshod Miraminov", prof = "JAVA DEVELOPER")
+                ListItem(name = "Mansur Ashrapov", prof = "C++ DEVELOPER")
+                ListItem(name = "Jahongir Qurbonov", prof = "PYTHON DEVELOPER")
+                ListItem(name = "Mubinjon Zoidov", prof = "KOTLIN DEVELOPER")
+                ListItem(name = "Vali Akhmedov", prof = "UX/UI")
+                ListItem(name = "Adbu Rahimov", prof = "PHP DEVELOPER")
+            }
+
+        }
+    }
+}
+
+@Composable
+private fun ListItem(name: String, prof: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        shape = RoundedCornerShape(15.dp),
+        //elevation = 5.dp
+    ) {
+        Box() {
             Row(
-                modifier = Modifier
-                    .background(Color.Gray)
-                    .fillMaxWidth().fillMaxHeight(0.5f)
+                verticalAlignment = Alignment.CenterVertically
             ) {
-                Column(
+                Image(
+                    painter = painterResource(id = R.drawable.image_1),
+                    contentDescription = "image",
+                    contentScale = ContentScale.Crop,
                     modifier = Modifier
-                        .background(Color.Red).fillMaxWidth(0.5f).fillMaxHeight(),
-                    verticalArrangement = Arrangement.SpaceAround,
-                    horizontalAlignment = Alignment.CenterHorizontally
+                        .padding(5.dp)
+                        .size(64.dp)
+                        .clip(CircleShape)
                 )
-                { //Ин контейнер мебошад ва барои мавқъи виджетро муайян кардан истифода мешавад
-                    Text(text = "Hello")
-                    Text(text = "Girona")
-                    Text(text = "Madrid")
-                }
-                Column(
-                    modifier = Modifier
-                        .background(Color.Green).fillMaxSize(),
-                    verticalArrangement = Arrangement.SpaceAround,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                )
-                { //Ин контейнер мебошад ва барои мавқъи виджетро муайян кардан истифода мешавад
-                    Text(text = "Hello")
-                    Text(text = "Girona")
-                    Text(text = "Madrid")
+                Column(Modifier.padding(start = 16.dp)) {
+                    Text(text = name)
+                    Text(text = prof)
                 }
             }
         }
     }
 }
-//@Preview (showBackground = true) //Ин функксия барои нишон додани виджет истифода мешавад
-//@Composable //Барои ҷеғ задани функсия истифода карда мешавад
-//fun NewText(){
-//    Text(text = "Hello World!")
-//}
